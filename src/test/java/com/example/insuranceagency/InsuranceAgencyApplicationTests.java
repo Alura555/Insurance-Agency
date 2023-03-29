a4f607c60159c6c9cd0f354bda81a1ea166d1e8e
Luiza Abdrashitova <abdrashitova.lr@gmail.com>
2023-03-29 09:40:12 +0300

    Initial Commit

---
+++ src/test/java/com/example/insuranceagency/InsuranceAgencyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.insuranceagency;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InsuranceAgencyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
