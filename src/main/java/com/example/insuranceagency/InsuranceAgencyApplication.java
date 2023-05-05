8ecfbb68e1a98ee47ae8fb1c89257e01415f7bcf
Luiza Abdrashitova <abdrashitova.lr@gmail.com>
2023-05-05 14:07:29 +0300

    Update Spring Boot application to exclude security autoconfiguration.

--- src/main/java/com/example/insuranceagency/InsuranceAgencyApplication.java
+++ src/main/java/com/example/insuranceagency/InsuranceAgencyApplication.java
@@ -2,8 +2,9 @@ package com.example.insuranceagency;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class })
 public class InsuranceAgencyApplication {
 
 	public static void main(String[] args) {
