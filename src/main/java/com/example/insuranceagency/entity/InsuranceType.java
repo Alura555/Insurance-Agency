212ed7d717c7dd9a67581b8d21eade11d2b62769
Luiza Abdrashitova <abdrashitova.lr@gmail.com>
2023-05-17 10:39:50 +0300

    Refactor import statements

--- src/main/java/com/example/insuranceagency/entity/InsuranceType.java
+++ src/main/java/com/example/insuranceagency/entity/InsuranceType.java
@@ -2,7 +2,12 @@ package com.example.insuranceagency.entity;
 
 import org.hibernate.annotations.Formula;
 
-import javax.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 import java.util.Objects;
 
 @Entity
